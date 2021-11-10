287a74bc95e2c85831b1d0b949c64e33d0c7d485
dwarfwiz <daerdq@mail.ru>
2021-11-10 22:50:05 +0300

    add js script

--- js/auth.js
+++ js/auth.js
@@ -1,18 +1,18 @@
-const inputLogin = document.querySelector("input[id=input_login]");
-const inputPassword = document.querySelector("input[id=input_password]");
 
-const buttonEnter = document.querySelector("button[id=login_button]")
+const inputLogin = document.querySelector("#input_login");
+const inputPassword = document.querySelector("#input_password");
+
+const buttonEnter = document.querySelector("#login_button")
 
 buttonEnter.addEventListener("click", function() {
     if(inputLogin.value == '') {
         inputLogin.style.backgroundColor = "#ff496c"
     } else {
         inputLogin.style.backgroundColor = "#ffffff"
-    }
-    if(inputPassword.value == '') {
-        inputPassword.style.backgroundColor = "#ff496c"
-    } else {
-        inputPassword.style.backgroundColor = "#ffffff"
+        if(inputPassword.value == '') {
+            inputPassword.style.backgroundColor = "#ff496c"
+        } else {
+            inputPassword.style.backgroundColor = "#ffffff"
+        }
     }
 })
-
